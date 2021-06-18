bace42342dce20176ab9daba0d5371f78d612bad
xmodulus <me@ziz.ninja>
2021-06-19 01:08:34 +0800

    add api helpers

--- src/api/quran.ts
+++ src/api/quran.ts
@@ -1,10 +1,26 @@
-export const getEdition = async (format: string, language: string, type: string): Promise<void> => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const getEdition = async (
+  format: string | null = "text", 
+  language: string | null = "en", 
+  type: string | null= "translation"
+): Promise<Record<string, any>> => {
   const response = await fetch(`http://api.alquran.cloud/v1/edition?format=${format}&language=${language}&type=${type}`);
   const json = await response.json();
 
   if (!response.ok) {
     throw new Error(json.error);
   }
-  return json;
+  return json.data;
 }
 
+export const getQuran = async (
+  edition: string | null = "quran-uthmani"
+): Promise<Record<string, any>> => {
+  const response = await fetch(`http://api.alquran.cloud/v1/quran/{${edition}`);
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.error);
+  }
+  return json.data;
+}
