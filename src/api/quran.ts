9ee7f5acdb0e3931b0d9357275ae6b75eea37557
xmodulus <me@ziz.ninja>
2021-06-19 13:10:58 +0800

    fix: quran API url

--- src/api/quran.ts
+++ src/api/quran.ts
@@ -36,7 +36,7 @@ export const getEdition = async (
 export const getQuran = async (
   edition: string | null = "quran-uthmani"
 ): Promise<Record<string, any>> => {
-  const response = await fetch(`http://api.alquran.cloud/v1/quran/{${edition}`);
+  const response = await fetch(`http://api.alquran.cloud/v1/quran/${edition}`);
   const json = await response.json();
 
   if (!response.ok) {
